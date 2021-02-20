2e51ea9d78a36dfc68d841674d967cc767707b1f
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-02-20 04:20:15 -0300

    chore: configure lint-staged

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -1,8 +1,18 @@
-import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-
-import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
+import { WebApplication, WithContext } from 'schema-dts'
+import { enableProdMode } from '@angular/core'
+import { AppModule } from './app/app.module'
+
+const p: WithContext<WebApplication> = {
+  '@context': 'https://schema.org',
+  '@type': 'WebApplication',
+  name: 'Speek - Simples e seguro',
+  about: 'Crie um link para conversar e pronto.',
+  description: 'Sua ligação funciona pessoa pra pessoa, sem servidores, sem persistência, direto ao ponto.',
+  additionalType: 'Liberdade em áudio e vídeo',
+  url: 'https://speek.video'
+}
 
 if (environment.production) {
   enableProdMode()
