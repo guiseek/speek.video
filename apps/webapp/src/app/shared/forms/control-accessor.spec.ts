84d264b6c71fc1842d4be6d5b4760c89d947ebdd
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-02-22 06:37:55 -0300

    build(cleanup): remove trash

--- apps/webapp/src/app/shared/forms/control-accessor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ControlAccessor } from './control-accessor'
-
-describe('ControlAccessor', () => {
-  it('should create an instance', () => {
-    expect(new ControlAccessor()).toBeTruthy()
-  })
-})
